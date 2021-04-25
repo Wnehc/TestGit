284690a761833091a4ca01a31f8a1cae252f51ae
eclipse_user <eclipse_user@atguigu.com>
2021-04-25 15:34:36 +0800

    test  conflict left

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class Happy {
 	public static void main(String[] args){
-		System.out.println("test commit***********");
+		System.out.println("left test commit***********");
 		System.out.println("test edit by myself");
 	}
 }
