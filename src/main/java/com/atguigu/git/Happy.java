abb2fa571e04685924d6b45e1480282857552a1c
eclipse_user <eclipse_user@atguigu.com>
2021-04-25 16:12:29 +0800

    edit by hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args){
 		System.out.println("left test commit***********");
 		System.out.println("test edit by myself");
+		System.out.println("hot_fix****");
 	}
 }
